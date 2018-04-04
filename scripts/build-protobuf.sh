52bd0033a88d9b9650cb20e8fdf1af855da5555e
John Erik Halse <johnerikhalse@gmail.com>
2018-04-04 10:22:02 +0200

    Update build-protobuf.sh

--- scripts/build-protobuf.sh
+++ scripts/build-protobuf.sh
@@ -9,7 +9,6 @@ wget -q https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1
 unzip protoc-3.5.1-linux-x86_64.zip
 rm protoc-3.5.1-linux-x86_64.zip
 wget -O - -q https://github.com/nlnwa/veidemann-api/archive/${API_VERSION}.tar.gz | tar --strip-components=2 -zx
-go get github.com/golang/protobuf/proto
-go get github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/golang/protobuf/protoc-gen-go
 
 bin/protoc -I. --go_out=plugins=grpc:../veidemann_api *.proto
