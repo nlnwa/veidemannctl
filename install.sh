5c467a973fdef93b20136140a1579c39fa61a7c8
johnerikhalse <johnerikhalse@gmail.com>
2019-03-06 10:15:40 +0100

    Added installation script for linux

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 VERSION=`curl -s -I https://github.com/nlnwa/veidemannctl/releases/latest | grep Location | sed -e 's/.*tag\/\([0-9\.]\+\).*$/\1/'`
 echo Installing veidemannctl v${VERSION}
 
-wget -q --show-progress https://github.com/nlnwa/veidemannctl/releases/download/${VERSION}/veidemannctl_${VERSION}_linux_amd64
-sudo cp veidemannctl_${VERSION}_linux_amd64 /usr/local/bin/veidemannctl
+curl -Lo veidemannctl https://github.com/nlnwa/veidemannctl/releases/download/${VERSION}/veidemannctl_${VERSION}_linux_amd64
+sudo mv veidemannctl /usr/local/bin/veidemannctl
 sudo chmod +x /usr/local/bin/veidemannctl
-rm veidemannctl_${VERSION}_linux_amd64
-sudo sh -c "veidemannctl completion > /etc/bash_completion.d/veidemannctl"
+
+# Install command completion
+sudo sh -c "/usr/local/bin/veidemannctl completion > /etc/bash_completion.d/veidemannctl"
