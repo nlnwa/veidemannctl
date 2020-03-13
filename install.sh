b03ced1ffe8ff29d671a38e75e448020e9cec69b
John Erik Halse <johnerikhalse@gmail.com>
2020-03-13 12:10:36 +0100

    fix typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=`curl -s -I https://github.com/nlnwa/veidemannctl/releases/latest | grep Location | sed -e 's/.*tag\/\([0-9\.]\+\).*$/\1/'`
+VERSION=`curl -s -I https://github.com/nlnwa/veidemannctl/releases/latest | grep location | sed -e 's/.*tag\/\([0-9\.]\+\).*$/\1/'`
 echo Installing veidemannctl v${VERSION}
 
 curl -L#o veidemannctl https://github.com/nlnwa/veidemannctl/releases/download/${VERSION}/veidemannctl_${VERSION}_linux_amd64
