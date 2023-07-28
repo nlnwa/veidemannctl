cb642cc5750e61724e9055db26340dee33e4f3e4
johnerikhalse <johnerikhalse@gmail.com>
2023-07-28 09:19:54 +0200

    Let command line flags for template and format have preference to definitions in stored queries

--- cmd/report/query_test.go
+++ cmd/report/query_test.go
@@ -1,6 +1,7 @@
 package report
 
 import (
+	reportV1 "github.com/nlnwa/veidemann-api/go/report/v1"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,79 +10,92 @@ func Test_queryCmdOptions_parseQuery(t *testing.T) {
 	tests := []struct {
 		name             string
 		fieldsFromFlags  *queryCmdOptions
+		args             []string
 		fieldsAfterParse *queryCmdOptions
 		want             *query
 		wantErr          assert.ErrorAssertionFunc
 	}{
 		{"template file",
-			&queryCmdOptions{queryOrFile: "testdata/template1.yaml"},
+			&queryCmdOptions{},
+			[]string{"testdata/template1.yaml"},
 			&queryCmdOptions{
-				queryOrFile: "testdata/template1.yaml",
+				format: "template",
 			},
 			&query{
 				Name:        "template1",
 				Description: "Example template\n",
 				Query:       "r.db('veidemann').table('config_crawl_entities')\n",
 				Template:    "{{.id}} {{.meta.name}}\n",
+				queryOrFile: "testdata/template1.yaml",
+				queryArgs:   make([]any, 0),
+				request: &reportV1.ExecuteDbQueryRequest{
+					Query: "r.db('veidemann').table('config_crawl_entities')\n",
+				},
 			},
 			assert.NoError},
 		{"template file with template flag",
 			&queryCmdOptions{
-				queryOrFile: "testdata/template1.yaml",
-				goTemplate:  "{{.id}}",
+				goTemplate: "{{.id}}",
 			},
+			[]string{"testdata/template1.yaml"},
 			&queryCmdOptions{
-				queryOrFile: "testdata/template1.yaml",
-				goTemplate:  "{{.id}}",
+				goTemplate: "{{.id}}",
+				format:     "template",
 			},
 			&query{
 				Name:        "template1",
 				Description: "Example template\n",
 				Query:       "r.db('veidemann').table('config_crawl_entities')\n",
-				Template:    "{{.id}} {{.meta.name}}\n",
+				Template:    "{{.id}}",
+				queryOrFile: "testdata/template1.yaml",
+				queryArgs:   make([]any, 0),
+				request: &reportV1.ExecuteDbQueryRequest{
+					Query: "r.db('veidemann').table('config_crawl_entities')\n",
+				},
 			},
 			assert.NoError},
 		{"template file with format flag",
 			&queryCmdOptions{
-				queryOrFile: "testdata/template1.yaml",
-				format:      "yaml",
+				format: "yaml",
 			},
+			[]string{"testdata/template1.yaml"},
 			&queryCmdOptions{
-				queryOrFile: "testdata/template1.yaml",
-				format:      "yaml",
+				format: "yaml",
 			},
 			&query{
 				Name:        "template1",
 				Description: "Example template\n",
 				Query:       "r.db('veidemann').table('config_crawl_entities')\n",
 				Template:    "{{.id}} {{.meta.name}}\n",
+				queryOrFile: "testdata/template1.yaml",
+				queryArgs:   make([]any, 0),
+				request: &reportV1.ExecuteDbQueryRequest{
+					Query: "r.db('veidemann').table('config_crawl_entities')\n",
+				},
 			},
 			assert.NoError},
 		{"nonexisting template file",
-			&queryCmdOptions{
-				queryOrFile: "missing.yaml",
-			},
-			&queryCmdOptions{queryOrFile: "missing.yaml"},
+			&queryCmdOptions{},
+			[]string{"missing.yaml"},
+			&queryCmdOptions{},
 			nil,
 			assert.Error},
 		{"query",
-			&queryCmdOptions{
-				queryOrFile: "r.db('veidemann').table('config_crawl_entities')",
-			},
-			&queryCmdOptions{
-				queryOrFile: "r.db('veidemann').table('config_crawl_entities')",
-			},
+			&queryCmdOptions{},
+			[]string{"r.db('veidemann').table('config_crawl_entities')"},
+			&queryCmdOptions{},
 			&query{
-				Name:        "",
-				Description: "",
-				Query:       "r.db('veidemann').table('config_crawl_entities')",
-				Template:    "",
+				Query: "r.db('veidemann').table('config_crawl_entities')",
+				request: &reportV1.ExecuteDbQueryRequest{
+					Query: "r.db('veidemann').table('config_crawl_entities')",
+				},
 			},
 			assert.NoError},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.fieldsFromFlags.parseQuery()
+			o := tt.fieldsFromFlags
+			got, err := o.parseQuery(tt.args)
 			if !tt.wantErr(t, err, "parseQuery()") {
 				return
 			}
